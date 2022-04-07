f40de0d5a96cda109f19aea2b0026aeee0df9a19
Danting Huang <serena.huangdanting@gmail.com>
2022-04-07 01:00:01 -0400

    display edits

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
+import VideoList from './VideoList.js';
+import exampleVideoData from '../data/exampleVideoData.js';
+import VideoPlayer from './VideoPlayer.js';
+import Search from './Search.js';
+
+
 var App = () => (
   <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+        <Search />
+
       </div>
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <VideoPlayer videos={exampleVideoData}/>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <VideoList videos={exampleVideoData}/>
       </div>
     </div>
   </div>
