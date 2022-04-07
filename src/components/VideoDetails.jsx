de9252f8a08e0196a7d340f6fbc161336fd39a32
Danting Huang <serena.huangdanting@gmail.com>
2022-04-07 11:42:24 -0400

    add video details component

---
+++ src/components/VideoDetails.jsx
@@ -0,0 +1,23 @@
+// import exampleVideoData from '../data/exampleVideoData.js';
+
+var VideoDetails = (props) => (
+  <div className="video-details">
+
+    <div >
+      <h4>some Video details</h4>
+      <div> Channel:  {props.video.snippet.channelTitle}</div>
+      <div> Publish Time: {props.video.snippet.publishTime}</div>
+    </div>
+
+  </div>
+);
+
+// PropTypes tell other developers what `props` a component expects
+// Warnings will be shown in the console when the defined rules are violated
+VideoDetails.propTypes = {
+  video: PropTypes.object
+};
+
+// In the ES6 spec, files are "modules" and do not share a top-level scope
+// `var` declarations will only exist globally where explicitly defined
+export default VideoDetails;
